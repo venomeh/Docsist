fe493af76572f22de67873480030a017fa91fb3a
Muhammad Abdullah Mahmood <muhammadabdullahmahmood@gmail.com>
2025-03-28 04:53:42 +0500

    finalize server with front/backend

--- backend/server.py
+++ backend/server.py
@@ -9,16 +9,14 @@ import uuid
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*":{"origins":"*"}})
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 async def generate_room_name():
     name = "room-" + str(uuid.uuid4())[:8]
     rooms = await get_rooms()
-    while name not in rooms:
+    while name in rooms:
         name = "room-" + str(uuid.uuid4())[:8]
     return name
-
 
 async def get_rooms():
     api = LiveKitAPI()
@@ -26,22 +24,23 @@ async def get_rooms():
     await api.aclose()
     return [room.name for room in rooms.rooms]
 
-
 @app.route("/getToken")
 async def get_token():
-    name = request.args.get("name", "my_name")
+    name = request.args.get("name", "my name")
     room = request.args.get("room", None)
     
     if not room:
-        room  = await generate_room_name()
+        room = await generate_room_name()
         
-    token = api.AccessToken(os.getenv("LIVEKIT_API_URL"),os.getenv("LIVEKIT_API_SECRET")\
+    token = api.AccessToken(os.getenv("LIVEKIT_API_KEY"), os.getenv("LIVEKIT_API_SECRET")) \
         .with_identity(name)\
         .with_name(name)\
-        .with_grants(api.VideoGrants(room_join=True,room=room)))
+        .with_grants(api.VideoGrants(
+            room_join=True,
+            room=room
+        ))
+    
+    return token.to_jwt()
 
-    return  token.to_jwt()
-
-
-if __name__  == "__main__":
-    app.run(host="0.0.0.0", port="5001", debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
